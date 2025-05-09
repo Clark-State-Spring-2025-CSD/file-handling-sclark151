e6f322aa10b0c351882e9a0db56ea2394802132d
Silas Clark <sopec151@gmail.com>
2025-05-09 19:08:27 -0400

    Updated

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,27 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+numInput = open("numbers.txt")
+
+sum = 0
+count = 0
+numList = []
+
+for curLine in numInput:
+    tempInt = int(curLine)
+    count += 1
+    sum += tempInt
+    numList.append(tempInt)
+
+numInput.close()
+
+max = max(numList)
+min = min(numList)
+average = sum / count
+
+print(f"Total amount of numbers: {count}")
+print(f"Sum of all numbers: {sum}")
+print(f"Average: {average}")
+print(f"Highest number: {max}")
+print(f"Lowest number: {min}")
--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,18 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+wordInput = open("words.txt")
+
+palindrome = 0
+wordList = []
+
+for curLine in wordInput:
+    tempStr = str(curLine.strip())
+    wordList.append(tempStr)
+    if tempStr == tempStr[::-1]:
+        palindrome += 1
+
+wordInput.close()
+
+print(f"There are {palindrome} palindromes in the file.")
